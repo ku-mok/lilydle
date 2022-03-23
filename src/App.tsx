26a4404fc23abec09609b45eb0cff13813416361
kueda <kei.mokusyun@gmail.com>
2022-03-23 22:27:57 +0900

    TODO記載

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,31 @@
 import { useCallback, useState } from "react";
-import AnswerColumn from "./components/AnswerColumn";
 import AnswerHistory from "./components/AnswerHistory";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import InputArea from "./components/InputArea";
 
 function App() {
-  const answer = "おたけすなお";
-  const [isAlternate, setIsAlternate] = useState(false);
-  const [answerHistory, setAnswerHistory] = useState<string[]>([
-    "あまのそらは",
-    "すずきちなみ",
-  ]);
-  const [inputingText, setInputingText] = useState("しらい");
+  // 解答候補一覧と現在の解答
+  const answerCandidates = [];
+  const answer = "おたけすなお"; // TODO: ゲームモードに応じてanswerを設定するロジックの実装
+  // ゲームモード
   const [mode, setMode] = useState<"daily" | "endless">("daily");
   const onModeChangeHandler = useCallback((mode: "daily" | "endless") => {
     setMode(mode);
     setAnswerHistory([]);
     setInputingText("");
   }, []);
+  // 回答履歴
+  // TODO: 回答履歴を積むロジックの実装
+  const [answerHistory, setAnswerHistory] = useState<string[]>([
+    "あまのそらは",
+    "すずきちなみ",
+  ]);
+  //  キーボードの入力モードと入力中のテキスト
+  const [isAlternate, setIsAlternate] = useState(false);
+  const [inputingText, setInputingText] = useState("しらい");
+  // 解答できる回数
   const MAX_ANSWER_COUNT = 6;
-  const answerCandidates = [];
   return (
     <>
       <Header mode={mode} onModeChange={onModeChangeHandler} />
@@ -37,10 +42,18 @@ function App() {
           isAlternate={isAlternate}
           onKanaToggle={(isAlternate) => setIsAlternate(isAlternate)}
           onBackSpaceClick={() => {
+            // TODO: 文字入力時の処理の実装
+            // InputtingTextを1文字削除する
             console.log("BS");
           }}
           onKanaClick={(kana) => {
+            // InputtingTextに文字を追加する
             console.log(kana);
+            // TODO: Submitの実装
+            // InputtingTextを空にする
+            // AnswerHistoryに解答履歴を追加する
+            // 正誤判定を行う
+            // 正解もしくは挑戦回数をオーバーした場合はモーダルを表示する
           }}
         />
         <div className="mt-6">
