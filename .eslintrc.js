12b8c3c6bb441c208df67db894729be3fa82f28a
kueda <kei.mokusyun@gmail.com>
2022-03-23 14:00:38 +0900

    init react

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/typescript",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "import", "react", "react-hooks"],
+  rules: { "react/react-in-jsx-scope": "off" },
+};
