74eaea520b6f3b7d701302748096b9f64219bcdc
kueda <kei.mokusyun@gmail.com>
2022-03-23 22:21:17 +0900

    メイン画面の実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,53 @@
-import Panel from "./components/Panel";
+import { useCallback, useState } from "react";
+import AnswerColumn from "./components/AnswerColumn";
+import AnswerHistory from "./components/AnswerHistory";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import InputArea from "./components/InputArea";
 
 function App() {
+  const answer = "おたけすなお";
+  const [isAlternate, setIsAlternate] = useState(false);
+  const [answerHistory, setAnswerHistory] = useState<string[]>([
+    "あまのそらは",
+    "すずきちなみ",
+  ]);
+  const [inputingText, setInputingText] = useState("しらい");
+  const [mode, setMode] = useState<"daily" | "endless">("daily");
+  const onModeChangeHandler = useCallback((mode: "daily" | "endless") => {
+    setMode(mode);
+    setAnswerHistory([]);
+    setInputingText("");
+  }, []);
+  const MAX_ANSWER_COUNT = 6;
+  const answerCandidates = [];
   return (
-    <div className="App">
-      <Panel status="green" character="あ" />
-    </div>
+    <>
+      <Header mode={mode} onModeChange={onModeChangeHandler} />
+      <div className="container md:w-3/12 sm:w-11/12 mx-auto">
+        <div className="container md:w-8/12 sm:w-full mx-auto">
+          <AnswerHistory
+            answers={answerHistory}
+            answer={answer}
+            inputingText={inputingText}
+            maxAnswerCount={MAX_ANSWER_COUNT}
+          />
+        </div>
+        <InputArea
+          isAlternate={isAlternate}
+          onKanaToggle={(isAlternate) => setIsAlternate(isAlternate)}
+          onBackSpaceClick={() => {
+            console.log("BS");
+          }}
+          onKanaClick={(kana) => {
+            console.log(kana);
+          }}
+        />
+        <div className="mt-6">
+          <Footer allAnswerCandidates={answerCandidates.length} />
+        </div>
+      </div>
+    </>
   );
 }
 
