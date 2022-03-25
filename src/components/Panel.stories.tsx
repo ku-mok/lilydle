f434230517b3be7442f6b275653d68f6eb52fbbf
kueda <kei.mokusyun@gmail.com>
2022-03-25 11:08:26 +0900

    Panelのstoriesの漏れをカバー

--- src/components/Panel.stories.tsx
+++ src/components/Panel.stories.tsx
@@ -1,24 +1,42 @@
 import Panel, { PanelProps } from "./Panel";
-import { Story, Meta } from "@storybook/react";
+import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
 
 export default {
   component: Panel,
-  title: "Panel",
-} as Meta;
+} as ComponentMeta<typeof Panel>;
 
-const Template: Story<PanelProps> = (args) => (
-  <div className="grid grid-cols-10 gap-1 w-4/12">
-    <Panel {...args} />
-  </div>
-);
-
-export const Correct = Template.bind({});
-Correct.args = {
-  status: "correct",
-  character: "あ",
+export const Correct: ComponentStoryObj<typeof Panel> = {
+  render: (args: PanelProps) => (
+    <div className="grid grid-cols-10 gap-1 w-4/12">
+      <Panel {...args} />
+    </div>
+  ),
+  args: {
+    status: "correct",
+    character: "あ",
+  },
 };
 
-export const notSubmitted = Template.bind({});
-notSubmitted.args = {
-  status: "notSubmitted",
+export const Candidate: ComponentStoryObj<typeof Panel> = {
+  ...Correct,
+  args: {
+    status: "candidate",
+    character: "あ",
+  },
+};
+
+export const Wrong: ComponentStoryObj<typeof Panel> = {
+  ...Correct,
+  args: {
+    status: "wrong",
+    character: "あ",
+  },
+};
+
+export const NotSubmitted: ComponentStoryObj<typeof Panel> = {
+  ...Correct,
+  args: {
+    status: "notSubmitted",
+    character: "あ",
+  },
 };
