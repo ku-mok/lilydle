26d55a6bce7e63e19ffa5d7b12ecf4bfd9ade0d3
kueda <kei.mokusyun@gmail.com>
2022-03-25 11:48:29 +0900

    storyshots導入

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,4 @@
 import "../src/index.css";
-import "tailwindcss/tailwind.css";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  moduleNameMapper: {
+    "^.+\\.css$": "<rootDir>/__mocks__/styleMock.js",
   },
   globals: {
     "ts-jest": {
