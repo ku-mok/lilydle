6804b4136ee9f381dfa5ea34846224bc66b43ed0
kueda <kei.mokusyun@gmail.com>
2022-03-25 15:36:30 +0900

    coverage出力設定

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 module.exports = {
   moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json"],
+  collectCoverageFrom: [
+    "src/**/*.{js,jsx,ts,tsx}",
+    "!src/main.tsx",
+    "!**/*.d.ts",
+    "!**/node_modules/**",
+  ],
   testEnvironment: "jsdom",
   transform: {
     "^.+\\.(ts|tsx)$": "ts-jest",
